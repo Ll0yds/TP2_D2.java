0eae256b747b9b56a910ef5abaa7fc0fe251d1df
Ll0yds <39957876+Ll0yds@users.noreply.github.com>
2018-07-14 13:47:09 +0200

    Add files via upload

---
+++ src/Maths.java
@@ -0,0 +1,21 @@
+
+public class Maths {
+public static int x;
+public static int y;
+public static double w;
+public static String z;
+public static void affichage(String a) {
+	System.out.println(a);
+}
+	public static void main(String[] args) {
+		 x = 1;
+		 y = 2;
+		 w = 3.0;
+		 z = "I love Maths";
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(w);
+affichage(z);
+	}
+
+}
